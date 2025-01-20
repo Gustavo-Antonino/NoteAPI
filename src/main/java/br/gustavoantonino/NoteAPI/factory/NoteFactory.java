b18eed565cbb473fcfa0549a3ffe7764cd197447
Gustavo-Antonino <guh.antonino@gmail.com>
2025-01-20 02:07:59 -0300

    Criando a classe NoteRepository para persistência no banco de dados e a classe NoteFactory.

---
+++ src/main/java/br/gustavoantonino/NoteAPI/factory/NoteFactory.java
@@ -0,0 +1,12 @@
+package br.gustavoantonino.NoteAPI.factory;
+
+
+import br.gustavoantonino.NoteAPI.entity.Note;
+import br.gustavoantonino.NoteAPI.dto.RequestNoteDTO;
+
+public class NoteFactory {
+
+    public static Note createNoteFromDTO(RequestNoteDTO requestNoteDTO) {
+        return new Note(requestNoteDTO);  // Utiliza o construtor que já existe na entidade Note
+    }
+}
